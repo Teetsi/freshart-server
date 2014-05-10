d48cc294574ac3aaadf408de347d615c4603893e
Viktor Kelemen <vkelemen@yammer-inc.com>
2014-05-10 12:57:20 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freshart::Application.config.secret_key_base = '5283d0cb59b9c9be40684641ece40c88124ec7c0883fdf4aa41763823a83be190daaecdde6f490769495cfaa7c061fdfda9fb5d3b1c0de339ae7600d42169fd3'
